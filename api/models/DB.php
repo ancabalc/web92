db91cc75e01588a4d901e150822bf66d1ea89642
siitwebcluj <paul.brie@scoalainformala.ro>
2017-04-07 18:49:30 +0000

    Add DB connection

--- api/models/DB.php
+++ api/models/DB.php
@@ -1,5 +1,17 @@
 <?php
     class  DB {
-         
-        
+
+        protected $dbh;
+    
+        function __construct() {
+
+            try {
+             $this->dbh = new PDO('mysql:host=' . DBHOST . ';dbname=' . DBNAME, 
+                              DBUSER, 
+                              DBPASS);
+            } catch (PDOException $e) {
+                print "Error!: " . $e->getMessage() . "<br/>";
+                die();
+            }
+        }
     }
