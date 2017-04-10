a71b43affeed7dad394a7ec8fa573f0281f43be5
Farkas Zsolt <farkas_zsolt@rocketmail.com>
2017-04-10 18:04:08 +0000

    started userUpdat js

---
+++ UI/views/users.js
@@ -0,0 +1,11 @@
+/*global $*/
+/*global Users*/
+$(document).ready(onHtmlLoaded);
+//always check if HTML is loaded before doing anything
+//HTML operaations on view
+
+function onHtmlLoaded(){
+    
+
+    
+}
